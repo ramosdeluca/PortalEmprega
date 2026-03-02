17eef307dbadbf4da4fbe29dc8aaf3e8e6c52ec2
Rogerio <ramosdeluca@gmail.com>
2026-03-02 09:03:18 -0300

    variaveis

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -6,5 +6,6 @@ const supabaseAnonKey = (import.meta as any).env.VITE_SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn('Supabase URL or Anon Key missing. Check your .env file.');
 }
-
+console.log("Supabase URL carregada:", !!supabaseUrl);
+console.log("Supabase Key carregada:", !!supabaseAnonKey);
 export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
