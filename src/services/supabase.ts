57773e536b2ccb1f6e20ec62a28f4c2863814d67
Rogerio <ramosdeluca@gmail.com>
2026-03-02 09:08:12 -0300

    variaveis

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -8,4 +8,7 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 console.log("Supabase URL carregada:", !!supabaseUrl);
 console.log("Supabase Key carregada:", !!supabaseAnonKey);
+// Isso vai mostrar se existem espaços no início ou fim
+console.log("URL:", `|${(import.meta as any).env.VITE_SUPABASE_URL}|`);
+console.log("KEY:", `|${(import.meta as any).env.VITE_SUPABASE_ANON_KEY}|`);
 export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
